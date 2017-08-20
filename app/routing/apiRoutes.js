873792fb84862b2d0543f39e3a6aee607fbfc277
Sofia Wicker <sofiazw@gmail.com>
2017-08-20 16:14:20 -0400

    update

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,35 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  
+
+  app.get("/api/tables", function(req, res) {
+ 
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+
+  });
+
+
+  app.post("/api/tables", function(req, res) {
+    }
+  });
+
+
+  app.post("/api/clear", function() {
+
+  });
+};
