4710ecc1a5b0d43b4f6914c014d96d514d1ddf85
Sofia Wicker <sofiazw@gmail.com>
2017-08-22 13:52:46 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,32 +4,31 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+// var tableData = require("../data/tableData");
+// var waitListData = require("../data/waitinglistData");
 
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+// // ===============================================================================
+// // ROUTING
+// // ===============================================================================
 
-module.exports = function(app) {
+// module.exports = function(app) {
   
 
-  app.get("/api/tables", function(req, res) {
+//   app.get("/api/tables", function(req, res) {
  
-  });
+//   });
 
-  app.get("/api/waitlist", function(req, res) {
+//   app.get("/api/waitlist", function(req, res) {
 
-  });
-
-
-  app.post("/api/tables", function(req, res) {
-    }
-  });
+//   });
 
 
-  app.post("/api/clear", function() {
+//   app.post("/api/tables", function(req, res) {
+//   });
 
-  });
-};
+
+//   app.post("/api/clear", function() {
+
+//   });
+// };
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require("express");
-
 var bodyParser = require("body-parser");
-
 var app = express();
 
 var PORT = process.env.PORT || 8080
@@ -11,9 +9,9 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes", app);
+require("./app/routing/htmlRoutes", app);
 
-app.list(PORT, function() {
+app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
