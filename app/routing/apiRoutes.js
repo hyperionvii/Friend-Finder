07e2119ddb789b565a0a3deaac65021efb75b379
Sofia Wicker <sofiazw@gmail.com>
2017-08-24 13:00:43 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,46 @@ module.exports = function(app) {
     res.json(surveyData);
   });
 
-//set it up to see who the friends are!
-//   app.post("/api/surveyData", function(req, res) {
-//   });
+  app.post("/api/surveyData", function(req, res) {
+
+    var surveyMatch =  
+    { name: "",
+    linkToPicture: "",
+    surveyResults: compatScore
+    };
+
+    // loop through all the friends in the survey list
+
+      //as you are looping through, compare the new survey answers to the answers in the surveyData
+
+
+    for(var i = 0; i <= surveyData.length; i++) {
+      surveyData[i]
+      var compatCount;
+      console.log(surveyData.length)
+
+      for(var e = 0; e < surveyData[i].surveyResults[e]; e++){
+
+        var set = surveyMatch.surveyResults[e];
+
+        if(surveyMatch.surveyResults[e] == surveyData[i].surveyResults[e]) {
+          var count;
+          compatCount = [];
+          count++;
+
+          console.log(surveyMatch.surveyResults[e]);
+
+          if( set == surveyMatch.surveyResults[e]) {
+            highestNumber.push(count);
+          };
+        };
+      };
+
+      var max = compatCount.indexOf(Math.max(compatCount));
+    }
+
+    surveyMatch.push(surveyData);
+  });
 
 
 };
