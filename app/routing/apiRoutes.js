fa8290475ccb57245f4b00f27eee4251bfcda94c
Sofia Wicker <sofiazw@gmail.com>
2017-09-18 12:12:21 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,3 @@
-
 var surveyData = require("../data/surveyData.js");
 
 module.exports = function(app) {
@@ -7,45 +6,11 @@ module.exports = function(app) {
     res.json(surveyData);
   });
 
+
+//
+
   app.post("/api/surveyData", function(req, res) {
-
-    var surveyMatch =  
-    { name: "",
-    linkToPicture: "",
-    difference: 1000
-    };
-
-    var userData = req.body;
-    var userScore = userData.surveyResults;
-
-    var totalDifference = 0;
-
-    // console.log(req.body);
-
-
-    for(var i = 0; i < surveyData.length; i++) {
-
-      console.log(surveyData[i]);
-
-      for(var e = 0; e < surveyData[i].surveyResults[e]; e++) {
-
-        // console.log(userScore);
-        // console.log(surveyData);
-
-        totalDifference += Math.abs(parseInt(userScore[e])) - parseInt(surveyData[i].surveyResults[e]);
-
-
-
-        if (totalDifference <= surveyMatch.difference) {
-
-          surveyMatch.name = surveyData[i].name;
-          surveyMatch.photo = surveyData[i].photo;
-          surveyMatch.difference = totalDifference;
-        };
-      };
-    };
-
-    surveyData.push(userData);
-    res.json(surveyMatch);
+    // res.json(surveyData);
   });
-};
+
+}
